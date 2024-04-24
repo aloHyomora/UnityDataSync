10cb9ff9dc007e89eaa837540b1f9c282cfa4f87
EJLimm <alohomoratolit@naver.com>
2024-04-24 18:32:16 +0900

    feat : socket programming
    #1
    python, Unity 간 소켓 통신을 통해 1회 데이터를 송수신 받기 성공
    
    VSCode 터미널엔 로그가 찍히고 Unity에서 일시 정지
    이후 VSCode 종료하면 Unity 일시 정지 풀리는 동시에 로그가 찍힘.

---
+++ DataSyncPythonProject/DataManager.py
@@ -0,0 +1,42 @@
+import socket
+import time
+
+def start_server():
+    print("Server is starting...")
+    host = '127.0.0.1' # 로컬 호스트
+    port = 55555
+    
+    # 소켓 객체 생성
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as so: # 네트워크 리소스 사용 후 자동으로 정리, 소켓 사용이 끝나면 자동으로 소켓을 닫음.
+        so.bind((host, port)) # 소켓에 주소 할당
+        so.listen()         # 클라이언트의 연결을 기다림
+        print("Server started. Waiting for connection...")
+        conn, addr = so.accept() # 연결 수락
+        
+        with conn:
+            print('Connected by', addr)
+            while True:
+                try:
+                    print("Read....")                            
+                    data = conn.recv(1024) # 한 번에 최대 1024 바이트의 데이터를 수신한다.
+                    if not data:
+                        print("No data received, attempting to reconnect...")
+                        break
+                    receivedNumber = int.from_bytes(data, byteorder='little', signed=True)
+                    float_number = float(receivedNumber) # 정수를 실수로 변환
+                    print("Received int : ", receivedNumber, "-> Conver to float : ", float_number)
+                
+                    modified_number = float_number * 2
+                    print("Send float value * 2 = ", modified_number)
+                
+                    float_data = str(modified_number).encode("utf-8") + b'\n' # 실수를 문자열로 변환 후 바이트로 인코딩하여 전송
+                    # bytearray(str(float_number * 2), 'utf-8') # 실수를 문자열로 변환 후 바이트 배열로 변환                        
+                    conn.sendall(float_data)
+                except Exception as e:
+                    print(f"Error during data processing : {e}. Retrying...")
+                    time.sleep(1)
+                
+        print("Connection Closed")
+        
+if __name__ == '__main__':
+    start_server()
---
+++ DataSyncUnityProject/Assets/PythonUnity/DataManager.cs
@@ -0,0 +1,94 @@
+using System;
+using System.Collections;
+using System.IO;
+using System.Net.Sockets;
+using UnityEngine;
+
+public class DataManager : MonoBehaviour
+{
+	[Range(1, 100)] public int numToSend = 0;
+	
+	private TcpClient _client;
+	private NetworkStream _stream;
+	private StreamReader _reader;
+	private void Start()
+	{
+		ConnectToServer();
+	}
+
+	void ConnectToServer()
+	{
+		try
+		{
+			_client = new TcpClient("localhost", 55555);
+			_stream = _client.GetStream();
+			_reader = new StreamReader(_stream);
+			Debug.Log($"Connected to server.");
+			
+			StartCoroutine(SendingDataRoutine());
+			StartCoroutine(ReceivingDataRoutine());
+		}
+		catch (Exception e)
+		{
+			Debug.LogError($"Socket Error : {e.Message}");
+			throw;
+		}
+	}
+
+	IEnumerator SendingDataRoutine()
+	{
+		while (true)
+		{
+			if (_client != null && _stream != null && _stream.CanWrite)
+			{
+				try
+				{
+					// 정수형 데이터 보내기(쓰기)
+					byte[] data = BitConverter.GetBytes(numToSend);
+					_stream.Write(data, 0, data.Length);
+					Debug.Log($"Data sent : {numToSend}");
+				}
+				catch (Exception e)
+				{
+					Debug.LogError($"Error sending data : {e.Message}");
+					break;
+				}
+			}
+			yield return new WaitForSeconds(1f);
+		}
+	}
+
+	IEnumerator ReceivingDataRoutine()
+	{
+		while (true)
+		{
+			if (_client != null && _stream != null && _stream.CanRead)
+			{
+				try
+				{
+					// 데이터 읽기
+					if (!_reader.EndOfStream) // 데이터에 줄 끝 표시가 없으면 EndOfStream이 true가 된다.
+					{
+						string message = _reader.ReadLine();
+						Debug.Log($"Received data : {message}");
+					}
+				}
+				catch (Exception e)
+				{
+					Debug.LogError($"Error receiving data : {e.Message}");
+					break;
+				}
+			}
+
+			yield return null;
+		}
+	}
+
+	private void OnDestroy()
+	{
+		// 자원 정리
+		if(_reader != null) _reader.Close();
+		if(_stream != null) _stream.Close();
+		if(_client != null) _client.Close();
+	}
+}
